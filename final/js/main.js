de17a87f3729d88b47b1e453fb9c7a69b968a842
ela komsul <elakomsul@hotmail.com>
2015-12-10 18:29:41 -0500

    present
    af

--- final/js/main.js
+++ final/js/main.js
@@ -1,1 +1,221 @@
+// $('.fadein img:gt(0)').hide();
 
+
+$(".fadein").hover( function() {
+		
+	// this gets called when you hover over
+	var $slideIn = $(this).find('.slide-in');
+	var $slideOut = $(this).find('.slide-out');
+
+	$slideIn
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '-227px',
+			opacity: 0
+		}, 350);
+
+}, function() {
+		
+	// this gets called when you hover off
+	var $slideIn = $(this).find('.slide-in');
+	var $slideOut = $(this).find('.slide-out');
+	
+	$slideIn
+		.animate({
+			left: '227px',
+			opacity: 0
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+	});
+
+$(".fadein2").hover( function() {
+		
+	// this gets called when you hover over
+	var $slideIn = $(this).find('.slide-in2');
+	var $slideOut = $(this).find('.slide-out2');
+
+	$slideIn
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '-227px',
+			opacity: 0
+		}, 350);
+
+}, function() {
+		
+	// this gets called when you hover off
+	var $slideIn = $(this).find('.slide-in2');
+	var $slideOut = $(this).find('.slide-out2');
+	
+	$slideIn
+		.animate({
+			left: '227px',
+			opacity: 0
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+	});
+
+$(".fadein3").hover( function() {
+		
+	// this gets called when you hover over
+	var $slideIn = $(this).find('.slide-in3');
+	var $slideOut = $(this).find('.slide-out3');
+
+	$slideIn
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '-227px',
+			opacity: 0
+		}, 350);
+
+}, function() {
+		
+	// this gets called when you hover off
+	var $slideIn = $(this).find('.slide-in3');
+	var $slideOut = $(this).find('.slide-out3');
+	
+	$slideIn
+		.animate({
+			left: '227px',
+			opacity: 0
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+	});
+$(".fadein4").hover( function() {
+		
+	// this gets called when you hover over
+	var $slideIn = $(this).find('.slide-in4');
+	var $slideOut = $(this).find('.slide-out4');
+
+	$slideIn
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '-227px',
+			opacity: 0
+		}, 350);
+
+}, function() {
+		
+	// this gets called when you hover off
+	var $slideIn = $(this).find('.slide-in4');
+	var $slideOut = $(this).find('.slide-out4');
+	
+	$slideIn
+		.animate({
+			left: '227px',
+			opacity: 0
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+	});
+$(".fadein5").hover( function() {
+		
+	// this gets called when you hover over
+	var $slideIn = $(this).find('.slide-in5');
+	var $slideOut = $(this).find('.slide-out5');
+
+	$slideIn
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '-227px',
+			opacity: 0
+		}, 350);
+
+}, function() {
+		
+	// this gets called when you hover off
+	var $slideIn = $(this).find('.slide-in5');
+	var $slideOut = $(this).find('.slide-out5');
+	
+	$slideIn
+		.animate({
+			left: '227px',
+			opacity: 0
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+	});
+$(".fadein6").hover( function() {
+		
+	// this gets called when you hover over
+	var $slideIn = $(this).find('.slide-in6');
+	var $slideOut = $(this).find('.slide-out6');
+
+	$slideIn
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '-227px',
+			opacity: 0
+		}, 350);
+
+}, function() {
+		
+	// this gets called when you hover off
+	var $slideIn = $(this).find('.slide-in6');
+	var $slideOut = $(this).find('.slide-out6');
+	
+	$slideIn
+		.animate({
+			left: '227px',
+			opacity: 0
+		}, 350);
+
+	$slideOut
+		.animate({
+			left: '0',
+			opacity: 1
+		}, 350);
+	});
